3046cc684d124a1edff3a93c7297cc83f7f25174
apodgaev <podgaev@web.de>
2017-01-15 01:42:43 +0100

    angular-cli error fixed, scss styles introduced, routing introduced

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,16 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'config', component: ConfigComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigComponent,
+    DashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
+		RouterModule.forRoot(appRoutes),
 		MaterialModule.forRoot(),
     BrowserModule,
     FormsModule,
