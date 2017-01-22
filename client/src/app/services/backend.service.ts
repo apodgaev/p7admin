8666f3513675d526ecb595774164b3412901a0ae
Aleksei AlexiuS Podgaev <podgaev@web.de>
2017-01-22 01:07:21 +0100

    error handling for backend service finished

--- client/src/app/services/backend.service.ts
+++ client/src/app/services/backend.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
 import { Router } from '@angular/router';
+import { MdSnackBar } from '@angular/material';
 
 export interface ResponseWrapper<T> {
   error   : number;
@@ -15,22 +16,28 @@ export class BackendService {
   constructor(
 		private http: Http,
 		private _router : Router,
+		public _snackBar: MdSnackBar
 	) { }
-
-	private handleError (error: Response) {
-    console.error(error);
-    return Observable.throw(error.status + " " + error.statusText || 'Server error');
-  }
 
 	private responseWrapper(request : Observable<Response>) {
 		return request.map(res =>  <ResponseWrapper<any>> res.json())
       .do((res : ResponseWrapper<any>) => {
 				console.log("Request result:", res);
 				if(res.error == -1) {
-					console.error("Auth error:", res.message);
+					let message = "Auth error: " + res.message;
+					this._snackBar.open(message, undefined, {
+			      duration: 2000,
+			    });
+					//console.error("Auth error:", res.message);
 					this._router.navigate(['/']);
 				}
-			}).catch(this.handleError);
+			}).catch((error: Response) => {
+				let message = error.status + " " + error.statusText || 'Server error';
+				this._snackBar.open(message, undefined, {
+		      duration: 2000,
+		    });
+		    return Observable.throw(message);
+			});
 	}
 
 	public post(url : string, body? : any, options? : RequestOptions) : Observable<any> {
